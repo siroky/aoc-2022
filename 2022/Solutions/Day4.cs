cc19a2d55bf23d126dc2c890e038fc59cadc567b
siroky.honza <siroky.honza@gmail.com>
2022-12-04 13:30:48 +0100

    Day 4 done

---
+++ 2022/Solutions/Day4.cs
@@ -0,0 +1,35 @@
+﻿using FuncSharp;
+
+namespace AOC.Solutions;
+
+public class Day4 : ISolver
+{
+    private static readonly ComparableTotalOrder<int> Intervals = new ComparableTotalOrder<int>();
+
+    public IEnumerable<int> Solve(IEnumerable<string> lines)
+    {
+        var intervalPairs = lines.Select(l => ParseIntervals(l));
+        var containingPairs = intervalPairs.Where(p =>
+            Intervals.Contains(p.Item1, p.Item2) ||
+            Intervals.Contains(p.Item2, p.Item1)
+        );
+        var overlappingPairs = intervalPairs.Where(p => Intervals.Intersects(p.Item1, p.Item2));
+
+        yield return containingPairs.Count();
+        yield return overlappingPairs.Count();
+    }
+
+    private (Interval<int>, Interval<int>) ParseIntervals(string line)
+    {
+        var intervals = line.Split(',');
+        return (ParseInterval(intervals[0]), ParseInterval(intervals[1]));
+    }
+
+    private Interval<int> ParseInterval(string i)
+    {
+        var bounds = i.Split('-');
+        var lowerBound = Int32.Parse(bounds[0]);
+        var upperBound = Int32.Parse(bounds[1]);
+        return Intervals.ClosedInterval(lowerBound, upperBound);
+    }
+}
