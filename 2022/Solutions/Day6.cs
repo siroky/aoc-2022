864d210519b33906d4b97fff9cd851ec8f0a19ca
siroky.honza <siroky.honza@gmail.com>
2022-12-06 11:44:54 +0100

    Missing file

---
+++ 2022/Solutions/Day6.cs
@@ -0,0 +1,28 @@
+﻿namespace AOC.Solutions;
+
+public class Day6 : ISolver
+{
+    public IEnumerable<string> Solve(IEnumerable<string> lines)
+    {
+        var line = lines.First().ToCharArray();
+        var startIndex = IndexOfDistinct(line, 4);
+        var messageIndex = IndexOfDistinct(line, 14);
+
+        yield return (startIndex + 4).ToString();
+        yield return (messageIndex + 14).ToString();
+    }
+
+    private int? IndexOfDistinct(char[] line, int count)
+    {
+        for (var i = 0; i <= line.Length - count; i++)
+        {
+            var chars = line.Skip(i).Take(count);
+            if (chars.Distinct().Count() == count)
+            {
+                return i;
+            }
+        }
+
+        return null;
+    }
+}
