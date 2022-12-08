5d5efe9d628f24ac5f904f4029347aa1787b26a1
siroky.honza <siroky.honza@gmail.com>
2022-12-08 23:38:27 +0100

    Readability improvements.

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -6,12 +6,15 @@ internal static class Program
 {
     static void Main(string[] args)
     {
-        for (var day = 1; day <= 25; day++)
+        for (var day = 25; day >= 1; day--)
         {
             var solverType = Type.GetType($"{nameof(AOC)}.{nameof(Solutions)}.Day{day}").ToOption();
-            var solver = solverType.Map(t => (ISolver)Activator.CreateInstance(t));
-
-            solver.Match(s => Solve(day, s));                
+            var solver = solverType.Map(t => (ISolver)Activator.CreateInstance(t)).GetOrNull();
+            if (solver != null)
+            {
+                Solve(day, solver);
+                break;
+            }             
         }
     }
 
--- 2022/Solutions/Day8.cs
+++ 2022/Solutions/Day8.cs
@@ -5,25 +5,23 @@ public class Day8 : ISolver
     public IEnumerable<string> Solve(IEnumerable<string> lines)
     {
         var forest = ParseForest(lines.ToList());
-        var visibility = Traverse(forest, (size, lineSizes) => lineSizes.Any(s => s >= size) ? 1 : 0, (a, b) => a + b);
-        var scores = Traverse(forest, (size, lineSizes) => lineSizes.TakeUntil(s => s >= size).Count(), (a, b) => a * b);
+        var blockedDirections = Traverse(forest, (size, lineSizes) => lineSizes.Any(s => s >= size) ? 1 : 0, (a, b) => a + b);
+        var scenicScores = Traverse(forest, (size, lineSizes) => lineSizes.TakeUntil(s => s >= size).Count(), (a, b) => a * b);
 
-        yield return visibility.Where(kv => kv.Value < 4).Count().ToString();
-        yield return scores.Values.Max().ToString();
+        yield return blockedDirections.Where(kv => kv.Value < 4).Count().ToString();
+        yield return scenicScores.Values.Max().ToString();
     }
 
-    private Dictionary<(int X, int Y), T> Traverse<T>(Forest forest, Func<int, IEnumerable<int>, T> lineResult, Func<T, T, T> aggregateResults)
+    private Dictionary<Vector, T> Traverse<T>(Forest forest, Func<int, IEnumerable<int>, T> evaluateLine, Func<T, T, T> aggregateResults)
     {
         var trees = forest.Trees;
-        var result = new Dictionary<(int, int), T>();
+        var result = new Dictionary<Vector, T>();
         foreach (var position in trees.Keys)
         {
-            var crossPositions = forest.Indexes.SelectMany(p => new[] { (X: p, Y: position.Y), (X: position.X, Y: p) });
-            var otherPositions = crossPositions.Where(p => p != position);
-            var neighbors = otherPositions.OrderBy(p => Math.Abs(position.X - p.X) + Math.Abs(position.Y - p.Y));
-
-            var lines = neighbors.GroupBy(p => (p.X.CompareTo(position.X), p.Y.CompareTo(position.Y)));
-            var results = lines.Select(l => lineResult(trees[position], l.Select(p => trees[p])));
+            var cuts = forest.Indexes.SelectMany(p => new[] { new Vector(p, position.Y), new Vector(position.X, p) });
+            var sortedCuts = cuts.Where(p => p != position).OrderBy(p => ManhattanDistance(p, position));
+            var lines = sortedCuts.GroupBy(p => Direction(p, position));
+            var results = lines.Select(d => evaluateLine(trees[position], d.Select(p => trees[p])));
 
             result[position] = results.Aggregate(aggregateResults);
         }
@@ -31,24 +29,34 @@ public class Day8 : ISolver
         return result;
     }
 
+    private int ManhattanDistance(Vector a, Vector b)
+    {
+        return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+    }
+
+    private Vector Direction(Vector from, Vector to)
+    {
+        return new Vector(from.X.CompareTo(to.X), from.Y.CompareTo(to.Y));
+    }
+
     private Forest ParseForest(List<string> lines)
     {
         var size = lines.Count();
         var indexes = Enumerable.Range(0, size);
 
-        var trees = new Dictionary<(int, int), int>();
+        var trees = new Dictionary<Vector, int>();
         foreach (var y in indexes)
         {
             var line = lines[y].ToCharArray();
             foreach (var x in indexes)
             {
-                trees[(x, y)] = line[x].ToInt();
+                trees[new Vector(x, y)] = line[x].ToInt();
             }
         }
 
         return new Forest(size, indexes, trees);
     }
 
-
-    public record Forest(int Size, IEnumerable<int> Indexes, Dictionary<(int X, int Y), int> Trees);
+    public record Vector(int X, int Y);
+    public record Forest(int Size, IEnumerable<int> Indexes, Dictionary<Vector, int> Trees);
 }
